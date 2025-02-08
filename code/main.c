0579bec314e3d5511a699a1786f19845d64c6495
Maxim UV <Maximumuvm at gmail dot com>
2025-02-08 12:19:25 +0300

    refactoring

--- code/main.c
+++ code/main.c
@@ -11,60 +11,59 @@
 
 void display_string(void)
 {
-  hd44780_i2c_setcursor(0, 0);
-  hd44780_i2c_sendstring("Hello");
-  hd44780_i2c_setcursor(0, 1);
-  hd44780_i2c_sendstring("From");
-  hd44780_i2c_setcursor(0, 2);
-  hd44780_i2c_sendstring("4 string");
-  hd44780_i2c_setcursor(0, 3);
-  hd44780_i2c_sendstring("Display");
-  hd44780_i2c_refresh();
-  os_set_timer_task(display_string, 500);
+    hd44780_i2c_setcursor(0, 0);
+    hd44780_i2c_sendstring("Hello");
+    hd44780_i2c_setcursor(0, 1);
+    hd44780_i2c_sendstring("From");
+    hd44780_i2c_setcursor(0, 2);
+    hd44780_i2c_sendstring("4 string");
+    hd44780_i2c_setcursor(0, 3);
+    hd44780_i2c_sendstring("Display");
+    hd44780_i2c_refresh();
+    os_set_timer_task(display_string, 500);
 }
 
 void scan_red_btn(void)
 {
-  if (GPIO_GET_PIN(BTN_RED_PORT, BTN_RED_PIN))
-    GPIO_PIN_RESET(LED_PORT, LED_RED_PIN);
-  else
-    GPIO_PIN_SET(LED_PORT, LED_RED_PIN);
+    if (GPIO_GET_PIN(BTN_RED_PORT, BTN_RED_PIN))
+        GPIO_PIN_RESET(LED_PORT, LED_RED_PIN);
+    else
+        GPIO_PIN_SET(LED_PORT, LED_RED_PIN);
 
-  os_set_task(scan_red_btn);
+    os_set_task(scan_red_btn);
 }
 
 void scan_grn_btn(void)
 {
-  if (GPIO_GET_PIN(BTN_GRN_PORT, BTN_GRN_PIN))
-    GPIO_PIN_RESET(LED_PORT, LED_GRN_PIN);
-  else
-    GPIO_PIN_SET(LED_PORT, LED_GRN_PIN);
-  os_set_task(scan_grn_btn);
+    if (GPIO_GET_PIN(BTN_GRN_PORT, BTN_GRN_PIN))
+        GPIO_PIN_RESET(LED_PORT, LED_GRN_PIN);
+    else
+        GPIO_PIN_SET(LED_PORT, LED_GRN_PIN);
+    os_set_task(scan_grn_btn);
 }
 
 int main(void)
 {
-  systemclock_init();
-  hardware_init();
-  systick_init();
-  i2c_init();
-  os_init();
+    systemclock_init();
+    hardware_init();
+    systick_init();
+    i2c_init();
+    os_init();
 
-  _delay_ms(500);
-  hd44780_i2c_init();
-  _delay_ms(100);
+    _delay_ms(500);
+    hd44780_i2c_init();
+    _delay_ms(100);
 
-  os_set_timer_task(display_string, 100);
-  os_set_task(scan_red_btn);
-  os_set_task(scan_grn_btn);
+    os_set_timer_task(display_string, 100);
+    os_set_task(scan_red_btn);
+    os_set_task(scan_grn_btn);
 
-  for (;;)
-  {
-    os_task_manager();
-  }
+    for (;;) {
+        os_task_manager();
+    }
 }
 
 void SysTick_Handler(void)
 {
-  os_timer_service();
+    os_timer_service();
 }
--- code/sys/hardware/hd44780_i2c.h
+++ code/sys/hardware/hd44780_i2c.h
@@ -10,26 +10,26 @@
 #error Undefined HD44780_I2C_ADDR
 #endif
 
-#define HD44780_I2C_RS              0b00000001
-#define HD44780_I2C_E               0b00000100
-#define HD44780_I2C_LIGHT_PIN       0b00001000
+#define HD44780_I2C_RS                  0b00000001
+#define HD44780_I2C_E                   0b00000100
+#define HD44780_I2C_LIGHT_PIN           0b00001000
 
-#define HD44780_I2C_PMSK            0b00001111
+#define HD44780_I2C_PMSK                0b00001111
 
-#define HD44780_I2C_MBLINK          0b00000001
-#define HD44780_I2C_MCUR            0b00000010
-#define HD44780_I2C_MDISPLAY_ON     0b00000100
+#define HD44780_I2C_MBLINK              0b00000001
+#define HD44780_I2C_MCUR                0b00000010
+#define HD44780_I2C_MDISPLAY_ON         0b00000100
 
-#define HD44780_I2C_PROCESSED       0b10000000
-#define HD44780_I2C_FREE            0b01111111
-#define HD44780_I2C_INIT1           0b01000000
-#define HD44780_I2C_INIT2           0b00100000
-#define HD44780_I2C_LIGHT_EN        0b00000001
-#define HD44780_I2C_LIGHT_PROC      0b00000010
-#define HD44780_I2C_LIGHT_MSK       0b00000011
+#define HD44780_I2C_PROCESSED           0b10000000
+#define HD44780_I2C_FREE                0b01111111
+#define HD44780_I2C_INIT1               0b01000000
+#define HD44780_I2C_INIT2               0b00100000
+#define HD44780_I2C_LIGHT_EN            0b00000001
+#define HD44780_I2C_LIGHT_PROC          0b00000010
+#define HD44780_I2C_LIGHT_MSK           0b00000011
 
-#define HD44780_I2C_LIGHT_ON        1
-#define HD44780_I2C_LIGHT_OFF       0
+#define HD44780_I2C_LIGHT_ON            1
+#define HD44780_I2C_LIGHT_OFF           0
 
 void hd44780_i2c_init(void);
 void hd44780_i2c_refresh(void);
