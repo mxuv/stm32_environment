1ba6628c8f69e248501e96053eb7b9f059d7678b
Maxx UV <mxuv dot dev at gmail dot com>
2025-08-12 14:28:46 +0300

    Merging nconv and str libs

--- src/sys/lib/str.c
+++ src/sys/lib/str.c
@@ -3,54 +3,147 @@
 
 #include "str.h"
 
-int stringlen (const char *str)
+char* sstringcopynz(char *dest, const char *src, int cnt)
 {
-  int len = 0;
-  while (*str) {
-    len++;
-    str++;
-  }
-  return len;
+    while (cnt) {
+        *dest = *src;
+        dest++;
+        src++;
+        cnt--;
+    }
+    return dest;
 }
 
-void stringcopy(char* dest, const char* src)
+char* stringcopynz(char *dest, const char *src)
 {
-  while(*src)
-  {
-    *dest = *src;
-    dest++;
-    src++;
-  }
+    while (*src) {
+        *dest = *src;
+        dest++;
+        src++;
+    }
+    return dest;
 }
 
-const char* skipchar(const char *str, char ch)
+void stringcopy(char *dest, const char *src)
 {
-  while (*str) {
-    if (*str != ch)
-      return str;
-    str++;
-  }
-  return NULL;
+    dest = stringcopynz(dest, src);
+    *dest = '\0';
 }
 
-const char* skipzeros(const char *str)
+void sstringcopy(char *dest, const char *src, int cnt)
 {
-  const char *s;
-
-  s = skipchar(str, '0');
-  if (s == NULL)
-    return str + stringlen(str) - 1;
-  else
-    return s;
+    dest = sstringcopynz(dest, src, cnt);
+    *dest = '\0';
 }
 
-void sstrcopy(char* dest, const char* src, uint8_t cnt)
+char* skipchar(const char *str, char ch)
 {
-  while(cnt)
-  {
-    *dest = *src;
-    dest++;
-    src++;
-    cnt--;
-  }
+    while (*str) {
+        if (*str != ch)
+            return (char*) str;
+        str++;
+    }
+    return NULL;
+}
+
+void str2fxp(char *str, int pindex, int bp, int ap)
+{
+    char buf[15];
+    char *p = buf;
+
+    if (*str == '-') {
+        *p = *str;
+        p++;
+    }
+
+    p = sstringcopynz(p, str + (pindex - (bp - 1)), bp);
+    *p++ = '.';
+    p = sstringcopynz(p, str + (pindex + 1), ap);
+    *p = '\0';
+    stringcopy(str, buf);
+}
+
+int stringlen(const char *str)
+{
+    int len = 0;
+    while (*str) {
+        len++;
+        str++;
+    }
+    return len;
+}
+
+void stripzeros(char *str)
+{
+    char *s;
+    if (*str == '-')
+        str++;
+
+    s = skipchar(str, '0');
+    if (s == NULL) {
+        *str++ = '0';
+        *str = '\0';
+        return;
+    } else {
+        if (*s == '.')
+            stringcopy(str, s - 1);
+        else
+            stringcopy(str, s);
+    }
+}
+
+static uint8_t getrank(uint8_t size)
+{
+    switch (size) {
+    case 1:
+        return 3;
+        break;
+    case 2:
+        return 5;
+        break;
+    case 4:
+    default:
+        return 10;
+        break;
+    }
+}
+
+void sint_strz(int size, int32_t number, char *str)
+{
+    uint32_t unum;
+    if (number < 0) {
+        unum = (uint32_t) (-(number + 1) + 1);
+        *str = '-';
+        str++;
+    } else {
+        unum = (uint32_t) number;
+    }
+    uint_strz(size, unum, str);
+}
+
+void uint_strz(int size, uint32_t number, char *str)
+{
+    uint8_t rank;
+
+    rank = getrank(size);
+    *(str + rank) = '\0';
+    str += rank - 1;
+    while (rank) {
+        *str = (char) (number % 10) + '0';
+        number /= 10;
+        str--;
+        rank--;
+    }
+}
+
+void uint_str(int size, uint32_t number, char *str)
+{
+    uint_strz(size, number, str);
+    stripzeros(str);
+}
+
+void sint_str(int size, uint32_t number, char *str)
+{
+    sint_strz(size, number, str);
+    stripzeros(str);
 }
--- src/sys/lib/str.h
+++ src/sys/lib/str.h
@@ -1,9 +1,19 @@
-#ifndef __STR_H__
-#define __STR_H__
+#ifndef _STR_H_
+#define _STR_H_
 
-void stringcopy(char* dest, const char* src);
-const char* skipchar(const char *str, char ch);
-const char* skipzeros(const char *str);
-void sstrcopy(char* dest, const char* src, uint8_t count);
+char* sstringcopynz(char *dest, const char *src, int cnt);
+char* stringcopynz(char *dest, const char *src);
+void stringcopy(char *dest, const char *src);
+void sstringcopy(char *dest, const char *src, int cnt);
+char* skipchar(const char *str, char ch);
+int stringlen(const char *str);
+
+void stripzeros(char *str);
+void str2fxp(char *str, int pindex, int bp, int ap);
+void sint_strz(int size, int32_t number, char *str);
+void uint_strz(int size, uint32_t number, char *str);
+void uint_str(int size, uint32_t number, char *str);
+void sint_str(int size, uint32_t number, char *str);
+
 
 #endif
